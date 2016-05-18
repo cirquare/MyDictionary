2bb67df9e04d95e376b8f318206d42bb88358318
ruey55664843 <rayyoyo2003@gmail.com>
2016-05-18 21:19:11 +0800

    fix some selection undefined error

--- src/components/WordReview.js
+++ src/components/WordReview.js
@@ -10,15 +10,16 @@ class WordReview extends Component {
     super(props, context);
     this.state = {
       WordList: {wordcards:[]},
-      test: []
+      test: [],
+      score: 0
     };
   }
 
-  handleTest(test){
-    const {topic} = test;
+  handleTest(test, i){
+    const {topic, selection} = test;
     return (
         <div>
-          <h4>{topic} {test.selection[0].title} {test.selection[1].title} {test.selection[2].title}</h4>
+          <h4>{topic} {selection[0].title} {selection[1].title} {selection[2].title}</h4>
         </div>
         )
   }
@@ -53,26 +54,53 @@ class WordReview extends Component {
       })
     }
   }
-  handleSelection(item, i, i_test){
+  handleTest2(test, i){
+    const {topic, selection} = test;
+    return(
+        <div>
+          <h4>{topic}</h4>
+          <ul>{selection.map(this.handleSelection,this, i)}</ul>
+        </div>
+        );
+  }
+  handleSelection(oneSelection, i, i_topic){
+    const {title, c} = oneSelection;
     return(
         <div>
           <input
             type = "checkbox"
-            checked = {item.c}
-            onChange = {this.handleSelectionChecked.bind(this)}
+            checked = {c}
+            onChange = {this.handleSelectionChecked.bind(this, i_topic)}
           />
-          <label>{item.title}</label>
+          <label>{title}</label>
         </div>
           );
   }
-  handleSelectionChecked(event, i, i_test){
+  handleSelectionChecked(event, i, i_topic){
     const {test} = this.state;
-    test[i_test].selection.splice(i,1,{
-      title: test[i_test].selection[i].title,
+    test[i_topic].selection.splice(i,1,{
+      title: test[i_topic].selection[i].title,
       c: event.target.checked
     });
     this.setState({
       test: test
+    });
+  }
+  handleAnswer(){
+    const {test, score} = this.state;
+    let score_temp = 0;
+    let countTopic = 0;
+    for(countTopic = 0; countTopic<3; countTopic++){
+      if(test[countTopic].selection[0].c)score_temp++;
+    }
+    if(score_temp === 0)score_temp = 0;
+    else if(score_temp === 1)score_temp = 60;
+    else if(score_temp === 2)score_temp = 80;
+    else if(score_temp === 2)score_temp = 100;
+    else score_temp = -1;
+
+    this.setState({
+      score: score_temp
     });
   }
   componentDidMount() {
@@ -82,15 +110,15 @@ class WordReview extends Component {
   }
 
   render() {
-    const {test} = this.state;
+    const {test, score} = this.state;
     const {wordcards} = this.state.WordList;
     return (
         <div className="container">
           <h1>Word Review Test</h1>
-          {test.map(this.handleTest.bind(this),this)}
-          <ul>{test[0].selection.map(this.handleSelection,this,0)}</ul>
-          <ul>{test[1].selection.map(this.handleSelection,this,1)}</ul>
-          <ul>{test[2].selection.map(this.handleSelection,this,2)}</ul>
+          {test.map(this.handleTest,this)}
+          {test.map(this.handleTest2,this)}
+          <button onclick = {this.handleAnswer.bind(this)}>submit</button>
+          <h4>{score}</h4>
         </div>
         );
   }
