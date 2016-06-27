20ba09dd40a8ac905b4e90d5791d6aaf5383e27a
CirQuare <yf186035@gmail.com>
2016-06-27 14:16:33 +0800

    Translation Function Completed

--- src/components/WordReview.js
+++ src/components/WordReview.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import 'babel-polyfill';
 import fetch from 'isomorphic-fetch';
-import Selection from './Selection'
+import Selection from './Selection';
+import Translation from './Translation';
 import './MyWordList.css';
 
 
@@ -13,20 +14,18 @@ class WordReview extends Component {
             WordList: {wordcards:[]},
             test: [],
             score: 0,
-            userInput: '',
         };
     }
 
     setWordList(temp){
         this.setState({WordList:temp});
     }
-    setTest(temp){
 
+    setTest(temp){
         let countTestNumber = 0;
         let countSelection = 0;
         let index = [0,0,0];
         let checkRepeat = 0;
-        //let shuffleSelection = 0;
         while(index[0] === index[1] || index[1] === index[2] || index[0] === index[2]){
             const { WordList, test } = this.state;
             const { wordcards } = WordList;
@@ -39,53 +38,48 @@ class WordReview extends Component {
             const { WordList, test } = this.state;
             const { wordcards } = WordList;
             const length = wordcards.length;
-            //shuffleSelection = Math.floor(Math.random()*3);
             this.setState({
                 test: test.concat({topic: wordcards[index[countTestNumber]].trans,
-                                  /*selection: [
-                                    {title: wordcards[index[0]].trans,c: false,testNumber:countTestNumber},
-                                    {title: wordcards[index[1]].trans,c: false,testNumber:countTestNumber},
-                                    {title: wordcards[index[2]].trans,c: false,testNumber:countTestNumber}
-                                    ],*/
                                   testID: countTestNumber+1,
                                   answer: wordcards[index[countTestNumber]].name,
-                                  userAns: wordcards[index[countTestNumber]].name,
+                                  userAns: "",
                 })
             })
         }
     }
-    handleTest(test, i){
-        const {topic, selection} = test;
-        const questionNumber = selection[0].testNumber + 1;
-        return(
-            <div>
-            <h4>Question {questionNumber} : {topic}</h4>
-            <ul>{selection.map(this.handleSelectionList,this)}</ul>
-            </div>
-        );
-    }
-
-    /*******************/
 
     handleTransTest(test,i){
         const {topic,testID,userAns} = test;
-        let {userInput} = this.state;
-        //const questionNumber = selection[0].testNumber + 1;
         const questionNumber = test.testID;
         return(
             <div>
             <h4>Question {questionNumber} : {topic}</h4>
             <form name="formName">
-            <input type="text" size="20" id="input" placeholder='請輸入對應英文'onChange={this.handleChange.bind(userInput,this)} />
+            <Translation
+                value = {userAns}
+                i = {i}
+                onChange = {this.handleChange.bind(this)}
+            />
             </form>
             </div>
         )
     }
 
-    handleChange(event,userInput){
+    handleChange(event,i){
+        const {test} = this.state;
+        const {testID} = test;
+        test.splice(i,1,{
+            topic: test[i].topic,
+            testID: test[i].testID,
+            answer: test[i].answer,
+            userAns: event.target.value
+        });
+
+       // console.log(event.target.value);
+        
         this.setState({
-            userInput: event.target.value 
-        })
+            test :test
+        });
     }
 
     handleTransScore(){
@@ -93,61 +87,18 @@ class WordReview extends Component {
         let score_temp = 0;
         let countTestNumber = 0;
         for(countTestNumber = 0; countTestNumber< 3; countTestNumber++){
-            //const { answer, userAns } = test[countTestNumber];
-            if(userInput[countTestNumber] === test[countTestNumber].answer)  score_temp++;
-        }
-        if(score_temp === 0)score_temp = 1111;
-        else if(score_temp === 1)score_temp = 60;
-        else if(score_temp === 2)score_temp = 80;
-        else if(score_temp === 3)score_temp = 100;
-        else score_temp = -1;
-        this.setState({
-            score: score_temp
-        });
-    }
-
-    /*******************/
-    handleSelectionList(oneSelection, i){
-        const {title, c, testNumber} = oneSelection;
-        return(
-            <Selection
-            index = {i}
-            testNumber = {testNumber}
-            title = {title}
-            c = {c}
-            onChange = {this.handleSelectionChecked.bind(this)}
-            />
-        );
-    }
-    handleSelectionChecked(event, i, testNumber){
-        const {test} = this.state;
-        test[testNumber].selection.splice(i,1,{
-            title: test[testNumber].selection[i].title,
-            c: event.target.checked,
-            testNumber: testNumber
-        });
-        this.setState({
-            test: test
-        });
-    }
-    handleScore(){
-        const {test, score} = this.state;
-        let score_temp = 0;
-        let countTopic = 0;
-        for(countTopic = 0; countTopic<3; countTopic++){
-            const {selection, answer} = test[countTopic];
-            if(selection[answer].c)score_temp = score_temp + 1;
+            if(test[countTestNumber].userAns === test[countTestNumber].answer)  score_temp++;
         }
         if(score_temp === 0)score_temp = 0;
         else if(score_temp === 1)score_temp = 60;
         else if(score_temp === 2)score_temp = 80;
         else if(score_temp === 3)score_temp = 100;
         else score_temp = -1;
-
         this.setState({
             score: score_temp
         });
     }
+
     componentDidMount() {
         fetch('/api/mywordlist')
         .then(function(res){return res.json()})
