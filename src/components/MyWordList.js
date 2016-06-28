cd855d6b3f83887973cdfdf172d02976c56cf555
ruey55664843 <rayyoyo2003@gmail.com>
2016-06-29 01:48:07 +0800

    input empty case

--- src/components/MyWordList.js
+++ src/components/MyWordList.js
@@ -12,7 +12,8 @@ class MyWordList extends Component {
     this.state = {
       WordList: {wordcards:[]},
       English: '',
-      Chinese: ''
+      Chinese: '',
+      ValidInput:true 
     };
   }
 
@@ -66,36 +67,40 @@ class MyWordList extends Component {
   }
   handleSubmit(event){
     const {WordList, Chinese, English} = this.state;
-    const countLength = WordList.wordcards.length + 1;
-    let countWordCards = 1;
-    for(;countWordCards<=countLength;countWordCards++){
-      if(countWordCards<=countLength-1){
-        WordList.wordcards.splice(countWordCards-1,1,{
-          name: WordList.wordcards[countWordCards-1].name,
-          trans: WordList.wordcards[countWordCards-1].trans,
-          testTime: WordList.wordcards[countWordCards-1].testTime,
-          number: WordList.wordcards[countWordCards-1].number,
-          total: countLength
-        })
-      }else{
-        WordList.wordcards.splice(countWordCards-1,0,{
-          name: English,
-          trans: Chinese,
-          testTime: 0,
-          number: countWordCards,
-          total: countLength
-        })
+    if(Chinese === '' || English === ''){
+      this.setState({ValidInput:false});
+    }else{
+      const countLength = WordList.wordcards.length + 1;
+      let countWordCards = 1;
+      for(;countWordCards<=countLength;countWordCards++){
+        if(countWordCards<=countLength-1){
+          WordList.wordcards.splice(countWordCards-1,1,{
+            name: WordList.wordcards[countWordCards-1].name,
+            trans: WordList.wordcards[countWordCards-1].trans,
+            testTime: WordList.wordcards[countWordCards-1].testTime,
+            number: WordList.wordcards[countWordCards-1].number,
+            total: countLength
+          })
+        }else{
+          WordList.wordcards.splice(countWordCards-1,0,{
+            name: English,
+            trans: Chinese,
+            testTime: 0,
+            number: countWordCards,
+            total: countLength
+          })
+        }
       }
+      this.setState({WordList:WordList});
+      fetch('/api/wordreview',{
+        method: 'post',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(WordList.wordcards),
+      });
     }
-    this.setState({WordList:WordList});
-    fetch('/api/wordreview',{
-      method: 'post',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json',
-      },
-      body: JSON.stringify(WordList.wordcards),
-    });
   }
   componentDidMount() {
     fetch('/api/mywordlist')
@@ -105,38 +110,71 @@ class MyWordList extends Component {
   }
 
   render() {
-    const {English, Chinese} = this.state;
+    const {English, Chinese, ValidInput} = this.state;
     const {wordcards} = this.state.WordList;
-    return (
-        <div className="container">
-          <h1 className="homepage-title"><b>My WordList</b></h1>
-            <div className="homepage-btn-crew">
-                <button type="button" className="btn btn-info homepage-btn">Info</button> &nbsp;
-                <button type="button" className="btn btn-success homepage-btn">Designer</button> &nbsp;
-                <Link to ={'/wordreview'}><button type="button" className="btn btn-warning homepage-btn">Selection Test</button></Link> &nbsp;
-                <Link to ={'/wordreview_trans'}><button type="button" className="btn btn-danger homepage-btn">Translation Test</button></Link> &nbsp;
-                <Link to ={'/reviewmode'}><button type="button" className="btn btn-default btn-change homepage-btn">Review</button></Link>
-            </div>
-            <form className="homepage-input-instr">
-                <div className="form-group input-btn-crew">   
-                    <input type="text" placeholder='English' className="homepage-input input-btn-change" value = {English} onChange = {this.handleNewEnglish.bind(this)}/> &nbsp;
-                    <input type="text" placeholder='Chinese' className="homepage-input input-btn-change" value = {Chinese} onChange = {this.handleNewChinese.bind(this)}/> &nbsp;
-                    <select name="Word Of Speech">
-                        <option value="VERB">Verb</option>
-                        <option value="NOUN">Noun</option>
-                        <option value="ADJECTIE">Adjective</option>
-                        <option value="ADVERB">Adverb</option>
-                        <option value="PREPOSITION">Preposition</option>
-                    </select> &nbsp;
-                    <input type="submit" value="Add To List" onClick = {this.handleSubmit.bind(this)} className="submit-btn"/>
-                </div>
-            </form>
-            
-            <span>
-                {wordcards.map(this.handleWordList.bind(this))}
-            </span>
-        </div>
-        );
+    if(ValidInput){ 
+      return (
+          <div className="container">
+            <h1 className="homepage-title"><b>My WordList</b></h1>
+              <div className="homepage-btn-crew">
+                  <button type="button" className="btn btn-info homepage-btn">Info</button> &nbsp;
+                  <button type="button" className="btn btn-success homepage-btn">Designer</button> &nbsp;
+                  <Link to ={'/wordreview'}><button type="button" className="btn btn-warning homepage-btn">Selection Test</button></Link> &nbsp;
+                  <Link to ={'/wordreview_trans'}><button type="button" className="btn btn-danger homepage-btn">Translation Test</button></Link> &nbsp;
+                  <Link to ={'/reviewmode'}><button type="button" className="btn btn-default btn-change homepage-btn">Review</button></Link>
+              </div>
+              <form className="homepage-input-instr">
+                  <div className="form-group input-btn-crew">   
+                      <input type="text" placeholder='English' className="homepage-input input-btn-change" value = {English} onChange = {this.handleNewEnglish.bind(this)}/> &nbsp;
+                      <input type="text" placeholder='Chinese' className="homepage-input input-btn-change" value = {Chinese} onChange = {this.handleNewChinese.bind(this)}/> &nbsp;
+                      <select name="Word Of Speech">
+                          <option value="VERB">Verb</option>
+                          <option value="NOUN">Noun</option>
+                          <option value="ADJECTIE">Adjective</option>
+                          <option value="ADVERB">Adverb</option>
+                          <option value="PREPOSITION">Preposition</option>
+                      </select> &nbsp;
+                      <input type="submit" value="Add To List" onClick = {this.handleSubmit.bind(this)} className="submit-btn"/>
+                  </div>
+              </form>
+              
+              <span>
+                  {wordcards.map(this.handleWordList.bind(this))}
+              </span>
+          </div>
+          );
+    }else{
+      return (
+          <div className="container">
+            <h1 className="homepage-title"><b>My WordList</b></h1>
+              <div className="homepage-btn-crew">
+                  <button type="button" className="btn btn-info homepage-btn">Info</button> &nbsp;
+                  <button type="button" className="btn btn-success homepage-btn">Designer</button> &nbsp;
+                  <Link to ={'/wordreview'}><button type="button" className="btn btn-warning homepage-btn">Selection Test</button></Link> &nbsp;
+                  <Link to ={'/wordreview_trans'}><button type="button" className="btn btn-danger homepage-btn">Translation Test</button></Link> &nbsp;
+                  <Link to ={'/reviewmode'}><button type="button" className="btn btn-default btn-change homepage-btn">Review</button></Link>
+              </div>
+              <form className="homepage-input-instr">
+                  <div className="form-group input-btn-crew">   
+                      <input type="text" placeholder='Empty!' className="homepage-input input-btn-change" value = {English} onChange = {this.handleNewEnglish.bind(this)}/> &nbsp;
+                      <input type="text" placeholder='Empty!' className="homepage-input input-btn-change" value = {Chinese} onChange = {this.handleNewChinese.bind(this)}/> &nbsp;
+                      <select name="Word Of Speech">
+                          <option value="VERB">Verb</option>
+                          <option value="NOUN">Noun</option>
+                          <option value="ADJECTIE">Adjective</option>
+                          <option value="ADVERB">Adverb</option>
+                          <option value="PREPOSITION">Preposition</option>
+                      </select> &nbsp;
+                      <input type="submit" value="Add To List" onClick = {this.handleSubmit.bind(this)} className="submit-btn"/>
+                  </div>
+              </form>
+              
+              <span>
+                  {wordcards.map(this.handleWordList.bind(this))}
+              </span>
+          </div>
+          );
+    }
   }
 }
 
